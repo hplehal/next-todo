d249d124681aa0b13af10fa24453df44c51863fa
Tej Lehal <tejlehal13@gmail.com>
2024-10-26 17:30:51 -0400

    Mutating server actions

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,9 +1,16 @@
+'use client'
+import { useTransition } from "react";
+import { CompleteTodo } from "@/utils/actions";
 
 const Todo = ({ todo }) => {
+    const [isPending, startTransition] = useTransition();
   return (
-    <div key={todo.id}>
-        <input type="checkbox" checked={todo.completed} />
-        {todo.content}
+    <div 
+        key={todo.id} 
+        className={`border border-black/20 cursor-pointer ${todo.isCompleted? 'line-through text-grey' : 'text-black'}`}
+        onClick={() => startTransition(() => CompleteTodo(todo.id))}>
+
+        <div>{todo.content}</div>
     </div>
   )
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,15 @@ import { revalidatePath } from 'next/cache'
 // SERVER ACTIONS - good for sideeffects
 import db from './db'
 
+export const CompleteTodo = async(id) => {
+    await db.todo.update({
+        where: { id },
+        data: { isCompleted: true },
+    })
+    
+    revalidatePath('/todos')
+}
+
 export const newTodo = async(formData) => {
     const todo = await db.todo.create({
         data: { 
